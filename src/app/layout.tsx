63f138a3d5d8e51235d37422f1d895a475cacc9e
Nishant Racherla <nishant.racherla@gmail.com>
2023-11-23 18:04:36 -0800

    Fix didnt work

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,48 +10,6 @@ export const metadata: Metadata = {
   title: "Nishant Racherla",
   description:
     "Nishant Racherla is a passionate Full-Stack Web Developer, skilled in React, Next.js, Redux, TypeScript, and more!",
-  openGraph: {
-    title: "Nishant Racherla",
-    description:
-      "Nishant Racherla is a passionate Full-Stack Web Developer, skilled in React, Next.js, Redux, TypeScript, and more!",
-    url: "https://portfolio-v2-t1lt.vercel.app/",
-    siteName: "Nishant Racherla",
-    images: [
-      {
-        url: "/opengraph-image.png",
-        width: 1200,
-        height: 630,
-      },
-      {
-        url: "/opengraph-image-min.png",
-        width: 800,
-        height: 420,
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Nishant Racherla",
-    description:
-      "Nishant Racherla is a passionate Full-Stack Web Developer, skilled in React, Next.js, Redux, TypeScript, and more!",
-    siteId: "1141343774951346177",
-    creator: "@NishantRacherla",
-    creatorId: "1141343774951346177",
-    images: [
-      {
-        url: "/twitter-image.png",
-        width: 1200,
-        height: 630,
-      },
-      {
-        url: "/twitter-image-min.png",
-        width: 800,
-        height: 420,
-      },
-    ],
-  },
 };
 
 export default function RootLayout({

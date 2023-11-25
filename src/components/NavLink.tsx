8b070967cb5997ac7d03c9f0630ccdcb688ea181
Nishant Racherla <nishant.racherla@gmail.com>
2023-11-24 18:14:29 -0800

    Fix nav links width

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -9,7 +9,7 @@ const NavLink = ({ title, href }: NavLinkProps) => {
   return (
     <a
       href={href}
-      className="flex items-center text-[#555] dark:text-neutral-500 hover:text-black hover:dark:text-white cursor-pointer transition duration-300 group"
+      className="w-max flex items-center text-[#555] dark:text-neutral-500 hover:text-black hover:dark:text-white cursor-pointer transition duration-300 group"
     >
       <span className="text-lg font-semibold">{title}</span>
       <FaArrowRight className="text-sm ml-2 group-hover:translate-x-2 transition-transform duration-300" />

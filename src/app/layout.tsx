5848213c7e3bfb7fab45924352856d6b1d81ac6b
Nishant Racherla <nishant.racherla@gmail.com>
2023-11-26 23:26:55 -0800

    Add vercel analytics, Change metadatabase url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,12 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/providers/ThemeProvider";
 import ToasterProvider from "@/providers/ToasterProvider";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  // CHANGE AFTER DOMAIN SHIFT
-  metadataBase: new URL("https://portfolio-v2-t1lt.vercel.app/"),
+  metadataBase: new URL("https://nishantracherla.com/"),
   title: "Nishant Racherla",
   description:
     "Nishant Racherla is a passionate Full-Stack Web Developer, skilled in React, Next.js, Redux, TypeScript, and more!",
@@ -30,6 +30,7 @@ export default function RootLayout({
       <body className={`${inter.className} bg-white dark:bg-[rgb(30,30,30)]`}>
         <ToasterProvider />
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );

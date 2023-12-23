c33c869696517da2ac853f996fe7f0f761037e70
Nishant Racherla <nishant.racherla@gmail.com>
2023-12-22 19:51:05 -0800

    Fix link underline color for light theme

--- src/components/Sections/AboutSection.tsx
+++ src/components/Sections/AboutSection.tsx
@@ -20,7 +20,7 @@ const AboutSection = () => {
           href="https://youtube.com/@mkbhd"
           target="_blank"
           rel="noreferrer"
-          className="font-semibold text-black dark:text-white hover:text-red-600 hover:dark:text-red-600 transition duration-300 border-b border-b-white hover:border-b-red-500"
+          className="font-semibold text-black dark:text-white hover:text-red-600 hover:dark:text-red-600 transition duration-300 border-b border-b-black dark:border-b-white hover:border-b-red-500"
         >
           MKBHD
         </a>
@@ -29,7 +29,7 @@ const AboutSection = () => {
           href="https://www.youtube.com/@LinusTechTips"
           target="_blank"
           rel="noreferrer"
-          className="font-semibold text-black dark:text-white hover:text-orange-600 hover:dark:text-orange-600 transition duration-300 border-b border-b-white hover:border-b-orange-600"
+          className="font-semibold text-black dark:text-white hover:text-orange-600 hover:dark:text-orange-600 transition duration-300 border-b border-b-black dark:border-b-white hover:border-b-orange-600"
         >
           LTT
         </a>

2a857f10be4a80a0cf4bb70a9626522148d0339f
Nishant Racherla <nishant.racherla@gmail.com>
2024-01-17 14:09:48 -0800

    tailwind config naming consistency

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -76,7 +76,7 @@ const config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        reveal: "reveal 0.7s ease-in-out"
+        "reveal": "reveal 0.7s ease-in-out"
       },
     },
   },

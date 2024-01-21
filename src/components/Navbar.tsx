b805a9f301c4f2a130c5bb5a5e9ff7f1c26cf89e
Nishant Racherla <nishant.racherla@gmail.com>
2024-01-21 14:04:09 -0800

    Convert navbar div to header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,12 @@ import ThemeToggleButton from "./ThemeToggleButton";
 
 const Navbar = () => {
   return (
-    <div className="sticky top-0 w-full p-6 md:p-8 flex justify-between items-center bg-white dark:bg-[rgb(17,17,16)]">
+    <header className="sticky top-0 w-full p-6 md:p-8 flex justify-between items-center bg-white dark:bg-[rgb(17,17,16)]">
       <Link href="/">
         <h1 className="text-neutral-800 dark:text-white font-bold">NR</h1>
       </Link>
       <ThemeToggleButton />
-    </div>
+    </header>
   );
 };
 
